1dd6a40453575b5eb5909177010ce86ba22e55a5
Joshua Ackerman <joshackerman757@gmail.com>
2023-11-22 14:13:25 -0500

    done

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const server = express();
 server.use(express.json());
 
 server.use('/api/projects', projectsRouter);
-server.get('/api/actions', actionsRouter); 
+server.use('/api/actions', actionsRouter); 
 
 
 module.exports = server;
